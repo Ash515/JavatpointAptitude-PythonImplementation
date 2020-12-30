8c7f2439dfbb5b938f2ca9df130247551fb5767c
Ash515 <aaashwin515@gmail.com>
2020-12-30 21:11:37 +0530

    Added Question10.py

---
+++ Average/Quetion10.py
@@ -0,0 +1,33 @@
+'''
+10) Four years ago, the average age of A and B was 20 years. If today average age of A, B and C is 25 years, what will be age of C after 7 years?
+32 years
+34 years
+36 years
+38 years
+'''
+AB_age=int(input("Enter the age before 4 years:"))
+ageofAB=AB_age*2
+presentageofAB=ageofAB+4*2
+ageofABC=25*3
+ageofc=ageofABC-presentageofAB
+ageofcafter7years=ageofc+7
+print("The age of C after 7 years is",ageofcafter7years,"years")
+
+#Output=> The age of C after 7 years is 34 years.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
